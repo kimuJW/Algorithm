369455285fe8e86cc0229a88ea48ae386218db4b
kimuJW <a01020532509@gmail.com>
2023-09-17 14:35:30 +0900

    codes

---
+++ bubbleSort.py
@@ -0,0 +1,27 @@
+def bubbleSort(a, n):
+        for I in range(n, 0, -1):
+            for j in range(1, I):
+                if a[j] > a[j+1]:
+                    a[j], a[j+1] = a[j+1], a[j]
+def checkSort(a, n):
+    isSorted = True
+    for i in range(1, n):
+        if (a[i] > a[i+1]):
+            isSorted = False
+        if (not isSorted):
+            break
+    if isSorted:
+        print("정렬 완료")
+    else:
+        print("정렬 오류 발생")
+import random, time
+N = 5000
+a = []
+a.append(None)
+for i in range(N):
+    a.append(random.randint(1, N))
+start_time = time.time()
+bubbleSort(a, N)
+end_time = time.time() - start_time
+print('버블 정렬의 실행 시간 (N = %d) : %0.3f'%(N, end_time))
+checkSort(a, N)
